9d3d5ea89dd59f56f2a99cf7fef0c33fdd00f85d
andywang <andywang@run-cloud.com.tw>
2025-11-28 17:17:56 +0800

    feat: add soft delete to all APIs

--- internal/database/models/book.go
+++ internal/database/models/book.go
@@ -1,11 +1,16 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Book struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Title     string    `json:"title"`
-	Author    string    `json:"author"`
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Title     string         `json:"title"`
+	Author    string         `json:"author"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
--- internal/database/models/user.go
+++ internal/database/models/user.go
@@ -1,12 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Email     string    `gorm:"unique" json:"email"`
-	Password  string    `json:"password"`
-	Role      string    `gorm:"type:varchar(20);default:'user'" json:"role"`
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Email     string         `gorm:"type:varchar(255);uniqueIndex:idx_users_email_deleted_at" json:"email"`
+	Password  string         `json:"password"`
+	Role      string         `gorm:"type:varchar(20);default:'user'" json:"role"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
